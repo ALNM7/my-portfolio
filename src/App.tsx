62e12c2efc825fc89e80714cfc81664be6923993
ALNM7 <alfredonava794@gmail.com>
2026-03-03 22:57:30 -0600

    new v

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Navbar } from './components/Navbar';
+import { Hero } from './components/Hero';
+import { About } from './components/About';
+import { Skills } from './components/Skills';
+import { Projects } from './components/Projects';
+import { Timeline } from './components/Timeline';
+import { Contact } from './components/Contact';
+
+export default function App() {
+  return (
+    <div className="min-h-screen">
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Skills />
+        <Projects />
+        <Timeline />
+        <Contact />
+      </main>
+    </div>
+  );
+}
